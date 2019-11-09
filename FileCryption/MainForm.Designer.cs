171cf26503ba909c8b67bec5552052aa9365478b
zcytxcbyz@163.com <zcytxcbyz@163.com>
2019-11-09 14:52:20 +0800

    Update

---
+++ FileCryption/MainForm.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace FileCryption
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxMode = new System.Windows.Forms.ComboBox();
+            this.comboBoxAlg = new System.Windows.Forms.ComboBox();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBoxMode
+            // 
+            this.comboBoxMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxMode.FormattingEnabled = true;
+            this.comboBoxMode.Items.AddRange(new object[] {
+            "加密",
+            "解密"});
+            this.comboBoxMode.Location = new System.Drawing.Point(58, 12);
+            this.comboBoxMode.Name = "comboBoxMode";
+            this.comboBoxMode.Size = new System.Drawing.Size(72, 20);
+            this.comboBoxMode.TabIndex = 0;
+            // 
+            // comboBoxAlg
+            // 
+            this.comboBoxAlg.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxAlg.FormattingEnabled = true;
+            this.comboBoxAlg.Items.AddRange(new object[] {
+            "AES-256"});
+            this.comboBoxAlg.Location = new System.Drawing.Point(58, 38);
+            this.comboBoxAlg.Name = "comboBoxAlg";
+            this.comboBoxAlg.Size = new System.Drawing.Size(72, 20);
+            this.comboBoxAlg.TabIndex = 1;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(215, 62);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 2;
+            this.buttonOpen.Text = "打开文件";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(59, 64);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(150, 21);
+            this.textBoxPath.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "路径";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "模式";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "算法";
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(134, 144);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 7;
+            this.buttonOK.Text = "确定";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(215, 144);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 8;
+            this.buttonCancel.Text = "取消";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "密钥";
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Location = new System.Drawing.Point(59, 91);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(150, 21);
+            this.textBoxKey.TabIndex = 10;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 179);
+            this.Controls.Add(this.textBoxKey);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.comboBoxAlg);
+            this.Controls.Add(this.comboBoxMode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "文件加密助手";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxMode;
+        private System.Windows.Forms.ComboBox comboBoxAlg;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxKey;
+    }
+}
+
---
+++ FileCryption/MainForm.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FileCryption
+{
+    public partial class MainForm : Form
+    {
+        public string SettingFilePath =
+    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\"
+    + Path.GetFileNameWithoutExtension(Application.ExecutablePath)
+    + ".dat";
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                comboBoxMode.SelectedIndex = 0;
+                comboBoxAlg.SelectedIndex = 0;
+                if (File.Exists(SettingFilePath))
+                {
+                    FileStream fileStream = new FileStream(SettingFilePath, FileMode.Open, FileAccess.Read);
+                    BinaryReader binaryReader = new BinaryReader(fileStream, Encoding.ASCII);
+                    this.Top = binaryReader.ReadInt32();
+                    this.Left = binaryReader.ReadInt32();
+                    comboBoxMode.SelectedIndex = binaryReader.ReadInt32();
+                    binaryReader.Close();
+                    fileStream.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK);
+            }
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                FileStream fileStream = new FileStream(SettingFilePath, FileMode.Create, FileAccess.Write);
+                BinaryWriter binaryWriter = new BinaryWriter(fileStream, Encoding.ASCII);
+                binaryWriter.Write(this.Top);
+                binaryWriter.Write(this.Left);
+                binaryWriter.Write(comboBoxMode.SelectedIndex);
+                binaryWriter.Close();
+                fileStream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK);
+            }
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.RestoreDirectory = true;
+                //ofd.Filter = "所有文件 (*.*)|*.*";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    textBoxPath.Text = ofd.FileName;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK);
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string FilePath = textBoxPath.Text.Trim();
+                if (File.Exists(FilePath))
+                {
+                    string Key = textBoxKey.Text;
+                    if (Key.Length > 32) throw new Exception("密钥长度不能超过32位。");
+                    int index = comboBoxMode.SelectedIndex;
+                    switch (index)
+                    {
+                        case 0:
+                            Encrypt(FilePath, Key);
+                            MessageBox.Show("加密成功。", this.Text);
+                            break;
+                        case 1:
+                            Decrypt(FilePath, Key);
+                            MessageBox.Show("解密成功。", this.Text);
+                            break;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("文件不存在。", this.Text, MessageBoxButtons.OK);
+                }
+            }
+            catch(CryptographicException)
+            {
+                if (comboBoxMode.SelectedIndex == 0)
+                {
+                    MessageBox.Show("加密失败", this.Text);
+                }
+                else
+                {
+                    MessageBox.Show("解密失败", this.Text);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK);
+            }
+        }
+
+        public void Encrypt(string path,string key)
+        {
+            FileStream fsread = new FileStream(path, FileMode.Open, FileAccess.Read);
+            byte[] data = new byte[fsread.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = (byte)fsread.ReadByte();
+            }
+            fsread.Close();
+            AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+            string sKey = key.PadRight(aes.Key.Length);
+            byte[] inputByteArray = data;
+            aes.Key = Encoding.Default.GetBytes(sKey);
+            aes.IV = Encoding.Default.GetBytes(sKey.Substring(0, aes.IV.Length));
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            byte[] retB = ms.ToArray();
+            FileStream fswrite = new FileStream(path, FileMode.Create, FileAccess.Write);
+            for(int i = 0; i < retB.Length; i++)
+            {
+                fswrite.WriteByte(retB[i]);
+            }
+            fswrite.Close();
+        }
+        public void Decrypt(string path, string key)
+        {
+            FileStream fsread = new FileStream(path, FileMode.Open, FileAccess.Read);
+            byte[] data = new byte[fsread.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = (byte)fsread.ReadByte();
+            }
+            fsread.Close();
+            AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+            string sKey = key.PadRight(aes.Key.Length);
+            byte[] inputByteArray = data;
+            aes.Key = Encoding.Default.GetBytes(sKey);
+            aes.IV = Encoding.Default.GetBytes(sKey.Substring(0, aes.IV.Length));
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            byte[] retB = ms.ToArray();
+            FileStream fswrite = new FileStream(path, FileMode.Create, FileAccess.Write);
+            for (int i = 0; i < retB.Length; i++)
+            {
+                fswrite.WriteByte(retB[i]);
+            }
+            fswrite.Close();
+        }
+    }
+}
